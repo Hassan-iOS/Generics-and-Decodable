056a563df4aceedf7114f5fcc631bc5981c4011d
Hassan Mostafa <hassanmostafa545@gmail.com>
2019-07-27 15:09:41 +0200

    Initial Commit

---
+++ Generics-Decodable-Demo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Generics-Decodable-Demo
+//
+//  Created by Hassan Mostafa on 7/27/19.
+//  Copyright © 2019 Minds. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
